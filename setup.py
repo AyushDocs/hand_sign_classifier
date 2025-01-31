5e87ca86a3252e3222c230f35d9535614e929dc1
AyushDocs <ayushalokdubey@gmail.com>
2025-01-31 18:52:21 +0530

    files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'hand_sign_classifier',
+    version= '0.0.0',
+    author= 'Ayush',
+    author_email= 'ayushalokdubey@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
